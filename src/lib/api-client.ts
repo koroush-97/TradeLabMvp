6751efb3f5ce6d134d00f7ab1269b6d9aa1c252e
koroush <kooroshbagheri97director@gmail.com>
2026-07-31 14:00:00 +0330

    feat(api): configure axios client with auth token

---
+++ src/lib/api-client.ts
@@ -0,0 +1,21 @@
+import axios from "axios";
+
+const apiClient = axios.create({
+  baseURL: process.env.NEXT_PUBLIC_API_BASE_URL,
+  headers: {
+    "Content-Type": "application/json",
+  },
+  timeout: 15_000,
+});
+
+apiClient.interceptors.request.use((config) => {
+  const token = localStorage.getItem("token");
+
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+
+  return config;
+});
+
+export default apiClient;
